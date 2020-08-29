0dd6b78f6fc86a9b5ba315aea38a577d76c86b79
TalentedTanishka <67682178+TalentedTanishka@users.noreply.github.com>
2020-08-29 17:20:19 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -10,8 +10,11 @@ class Ground {
     }
     display(){
       var pos =this.body.position;
+      push();
       rectMode(CENTER);
-      fill("brown");
+      strokeWeight(4);
+      fill("white");
       rect(pos.x, pos.y, this.width, this.height);
+      pop();
     }
   };
